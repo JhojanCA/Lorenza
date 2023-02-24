3a65134aeb843b03d11e25ae2d7a3c374ec4fa30
JhojanCA <jhojanalexca12@gmail.com>
2023-02-24 15:00:58 -0500

    proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarraComponent } from './barra/barra.component';
+import { SliderComponent } from './slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
